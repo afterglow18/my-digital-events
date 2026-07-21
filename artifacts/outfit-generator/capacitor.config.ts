15f903182d5c5cbf52b26f9692ed766af54013f5
afterglow18 <afterglowtanning18@gmail.com>
2026-07-21 23:21:14 +0000

    Set app display name to 'My Events'

--- artifacts/outfit-generator/capacitor.config.ts
+++ artifacts/outfit-generator/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.mydigitalevents.app',
-  appName: 'My Digital Events',
+  appName: 'My Events',
   webDir: 'dist/public',
 
   // -------------------------------------------------------------------------
